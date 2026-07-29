4c556c6cb4d4b7a47ad4c1f5bddcd1683b8fc09a
Kaka fadilah <kakafadilah237@gmail.com>
2026-07-29 10:37:37 +0700

    fix: chown storage after artisan commands to prevent permission denied in Render

--- startup.sh
+++ startup.sh
@@ -10,4 +10,7 @@ php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
 
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 nginx -g "daemon off;"
