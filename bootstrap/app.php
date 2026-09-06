bcb282e5196e2f06d85e0a209f0fbd73d1b4f7c5
Kaka fadilah <kakafadilah237@gmail.com>
2026-09-06 13:54:44 +0700

    fix: add HEADER_X_FORWARDED_PROTO to TrustProxies so Laravel detects HTTPS behind Cloudflare

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT);
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
 
         $middleware->web(append: [
             ContentSecurityPolicy::class,
