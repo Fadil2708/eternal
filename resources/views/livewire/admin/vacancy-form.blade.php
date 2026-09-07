42b59aca359cf6bb35eaae9a0d18f30805dbd3bc
Kaka fadilah <kakafadilah237@gmail.com>
2026-09-07 12:11:16 +0700

    fix: trix editor numbering by using wire:ignore.self instead of parent wire:ignore

--- resources/views/livewire/admin/vacancy-form.blade.php
+++ resources/views/livewire/admin/vacancy-form.blade.php
@@ -35,16 +35,16 @@
                         @error('quota') <div class="field-error">{{ $message }}</div> @enderror
                     </div>
                 </div>
-                <div wire:ignore class="field">
+                <div class="field">
                     <label>Deskripsi</label>
-                    <input type="hidden" id="description-input" value="{{ $description }}">
-                    <trix-editor input="description-input" x-data x-on:trix-change="$wire.set('description', $event.target.value)"></trix-editor>
+                    <input type="hidden" id="description-input" value="{{ $description }}" wire:ignore>
+                    <trix-editor input="description-input" wire:ignore.self x-data x-on:trix-change="$wire.set('description', $event.target.value)"></trix-editor>
                     @error('description') <div class="field-error">{{ $message }}</div> @enderror
                 </div>
-                <div wire:ignore class="field">
+                <div class="field">
                     <label>Kualifikasi</label>
-                    <input type="hidden" id="qualifications-input" value="{{ $qualifications }}">
-                    <trix-editor input="qualifications-input" x-data x-on:trix-change="$wire.set('qualifications', $event.target.value)"></trix-editor>
+                    <input type="hidden" id="qualifications-input" value="{{ $qualifications }}" wire:ignore>
+                    <trix-editor input="qualifications-input" wire:ignore.self x-data x-on:trix-change="$wire.set('qualifications', $event.target.value)"></trix-editor>
                     @error('qualifications') <div class="field-error">{{ $message }}</div> @enderror
                 </div>
                 <div class="form-row-3">
